5d8549dbc3c90bd26f7e14977edbfd5764dbff71
Keir <keir.xen@gmail.com>
2011-12-20 14:50:33 +0000

    libdisk: Support Sensible Software "SOS" format

---
+++ libdisk/sensible.c
@@ -0,0 +1,108 @@
+/*
+ * disk/sensible.c
+ * 
+ * Custom format as used by various Sensible Software releases:
+ *   Cannon Fodder
+ *   Mega Lo Mania
+ *   Wizkid
+ * 
+ * Written in 2011 by Keir Fraser
+ * 
+ * RAW TRACK LAYOUT:
+ *  u32 0x44894489 :: Sync
+ *  u8  'S','O','S','6'
+ *  u32 csum
+ *  u32 tracknr^1
+ *  u32 data[12*512/4]
+ * MFM encoding of sectors:
+ *  All odd bits, followed by all even bits.
+ * 
+ * TRKTYP_sensible data layout:
+ *  u8 sector_data[12*512]
+ */
+
+#include <libdisk/util.h>
+#include "private.h"
+
+#include <arpa/inet.h>
+
+#define SOS_SIG 0x534f5336u
+
+static void *sensible_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    char *block;
+
+    while (stream_next_bit(s) != -1) {
+
+        uint32_t raw_dat[2*(12+ti->len)/4], csum = 0;
+        unsigned int i;
+
+        if (s->word != 0x44894489)
+            continue;
+
+        ti->data_bitoff = s->index_offset - 31;
+
+        if (stream_next_bytes(s, raw_dat, sizeof(raw_dat)) == -1)
+            goto fail;
+        mfm_decode_bytes(MFM_odd_even, 12+ti->len, raw_dat, raw_dat);
+
+        if ((ntohl(raw_dat[0]) != SOS_SIG) ||
+            ((uint8_t)ntohl(raw_dat[2]) != (tracknr^1)))
+            continue;
+
+        for (i = 0; i < ARRAY_SIZE(raw_dat)/2; i++)
+            csum += ntohl(raw_dat[i]);
+        csum -= ntohl(raw_dat[1]) * 2;
+        if (csum != 0)
+            continue;
+
+        block = memalloc(ti->len);
+        memcpy(block, &raw_dat[3], ti->len);
+        ti->valid_sectors = (1u << ti->nr_sectors) - 1;
+        return block;
+    }
+
+fail:
+    return NULL;
+}
+
+static void sensible_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    uint32_t *dat = (uint32_t *)ti->dat, csum;
+    unsigned int i, enc;
+
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44894489);
+
+    csum = SOS_SIG + (tracknr ^ 1);
+    for (i = 0; i < ti->len/4; i++)
+        csum += ntohl(dat[i]);
+
+    for (i = 0; i < 2; i++) {
+        enc = (i == 0) ? MFM_odd : MFM_even;
+        tbuf_bits(tbuf, SPEED_AVG, enc, 32, SOS_SIG);
+        tbuf_bits(tbuf, SPEED_AVG, enc, 32, csum);
+        tbuf_bits(tbuf, SPEED_AVG, enc, 32, tracknr^1);
+        tbuf_bytes(tbuf, SPEED_AVG, enc, ti->len, dat);
+    }
+}
+
+struct track_handler sensible_handler = {
+    .bytes_per_sector = 12*512,
+    .nr_sectors = 1,
+    .write_mfm = sensible_write_mfm,
+    .read_mfm = sensible_read_mfm
+};
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "Linux"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
