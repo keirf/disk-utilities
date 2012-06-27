1d4a2277959062331eb5c3980977cea9f218c2b8
Keir <keir.xen@gmail.com>
2012-06-27 10:45:01 +0100

    libdisk: Split longtrack protection into PROTEC and Gremlin.
    Added to mfmparse_formats:
     Motorbike Madness
     The Munsters

--- libdisk/longtrack.c
+++ libdisk/longtrack.c
@@ -5,18 +5,18 @@
  * 
  * Written in 2011 by Keir Fraser
  * 
- * Type 0: Amnios, Archipelagos
+ * TRKTYP_protec_longtrack: PROTEC protection track, used on many releases
  *  u16 0x4454
  *  u8 0x33 (encoded in-place, 1000+ times, to track gap)
  *  Track is checked to be >= 107200 bits long
  * 
- * Type 1: Lotus I/II
+ * TRKTYP_gremlin_longtrack: Lotus I/II
  *  u16 0x4124,0x4124
  *  Rest of track is (MFM-encoded) zeroes
  *  Track is checked to be >= 102400 bits long
  * 
- * TRKTYP_longtrack data layout:
- *  u16 type
+ * TRKTYP_* data layout:
+ *  No data (all track formats are fixed format with no key/real data)
  */
 
 #include <libdisk/util.h>
@@ -34,66 +34,66 @@ static int check_sequence(struct stream *s, unsigned int nr, uint8_t byte)
     return !nr;
 }
 
-static void *longtrack_write_mfm(
+static void *protec_longtrack_write_mfm(
     struct disk *d, unsigned int tracknr, struct stream *s)
 {
     struct track_info *ti = &d->di->track[tracknr];
-    uint16_t *dat = memalloc(2);
 
     while (stream_next_bit(s) != -1) {
-
-        if (s->word == 0x4454a525) {
-            if (!check_sequence(s, 1000, 0x33))
-                continue;
-            ti->data_bitoff = s->index_offset - 31;
-            ti->total_bits = 110000; /* long enough */
-            *dat = 0;
-            goto found;
-        }
-
-        if (s->word == 0x41244124) {
-            if (!check_sequence(s, 1000, 0x00))
-                continue;
-            ti->data_bitoff = s->index_offset - 31;
-            ti->total_bits = 105500; /* long enough */
-            *dat = 1;
-            goto found;
-        }
+        ti->data_bitoff = s->index_offset - 31;
+        if ((s->word != 0x4454a525) || !check_sequence(s, 1000, 0x33))
+            continue;
+        ti->total_bits = 110000; /* long enough */
+        return memalloc(0);
     }
 
-    memfree(dat);
     return NULL;
-
-found:
-    *dat = htons(*dat);
-    ti->len = 2;
-    return dat;
 }
 
-static void longtrack_read_mfm(
+static void protec_longtrack_read_mfm(
     struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
 {
-    struct track_info *ti = &d->di->track[tracknr];
-    uint16_t *dat = (uint16_t *)ti->dat;
     unsigned int i;
 
-    switch (ntohs(*dat)) {
-    case 0:
-        tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, 0x4454);
-        for (i = 0; i < 6000; i++)
-            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x33);
-        break;
-    case 1:
-        tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x41244124);
-        for (i = 0; i < 6000; i++)
-            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0);
-        break;
-    }
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, 0x4454);
+    for (i = 0; i < 6000; i++)
+        tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x33);
 }
 
-struct track_handler longtrack_handler = {
-    .write_mfm = longtrack_write_mfm,
-    .read_mfm = longtrack_read_mfm
+struct track_handler protec_longtrack_handler = {
+    .write_mfm = protec_longtrack_write_mfm,
+    .read_mfm = protec_longtrack_read_mfm
+};
+
+static void *gremlin_longtrack_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+
+    while (stream_next_bit(s) != -1) {
+        ti->data_bitoff = s->index_offset - 31;
+        if ((s->word != 0x41244124) || !check_sequence(s, 1000, 0x00))
+            continue;
+        ti->total_bits = 105500; /* long enough */
+        return memalloc(0);
+    }
+
+    return NULL;
+}
+
+static void gremlin_longtrack_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    unsigned int i;
+
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x41244124);
+    for (i = 0; i < 6000; i++)
+        tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0);
+}
+
+struct track_handler gremlin_longtrack_handler = {
+    .write_mfm = gremlin_longtrack_write_mfm,
+    .read_mfm = gremlin_longtrack_read_mfm
 };
 
 /*
