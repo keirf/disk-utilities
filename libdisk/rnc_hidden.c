36a1929fc1c8c40f838156ace2ed4a88cf36f048
Keir <keir.xen@gmail.com>
2012-07-07 14:48:34 +0100

    libdisk: Support RNC hidden-sector format, as used on Time.

---
+++ libdisk/rnc_hidden.c
@@ -0,0 +1,156 @@
+/*
+ * disk/rnc_hidden.c
+ * 
+ * Small sectors hidden alongside AmigaDOS track data.
+ * 
+ * Written in 2012 by Keir Fraser
+ */
+
+#include <libdisk/util.h>
+#include "private.h"
+
+#include <arpa/inet.h>
+
+static const uint16_t sync_list[] = {
+    0x8912, 0x8911, 0x8914, 0x8915 };
+#define NR_SYNCS 4
+
+unsigned int bit_weight(uint8_t *p, unsigned int nr)
+{
+    unsigned int nr_ones = 0;
+    while (nr--) {
+        uint8_t b = *p++;
+        while (b) {
+            if (b & 1)
+                nr_ones++;
+            b >>= 1;
+        }
+    }
+    return nr_ones;
+}
+
+static void *rnc_hidden_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    char *ablk, *block = NULL;
+    uint8_t raw[40], sig[10], sigs[4][10], nr_sigs = 0;
+    uint32_t valid_blocks = 0;
+    unsigned int i, j, found, sec, nr_ones;
+
+    init_track_info(ti, TRKTYP_amigados);
+    ablk = handlers[TRKTYP_amigados]->write_mfm(d, tracknr, s);
+    if ((ablk == NULL) || (ti->type != TRKTYP_amigados))
+        goto out;
+
+    stream_reset(s, tracknr);
+    stream_next_index(s);
+
+    while ((stream_next_bit(s) != -1) && (nr_sigs < NR_SYNCS)) {
+
+        for (sec = 0; sec < NR_SYNCS; sec++)
+            if ((uint16_t)s->word == sync_list[sec])
+                break;
+        if ((sec == NR_SYNCS) || (valid_blocks & (1u<<sec)))
+            continue;
+
+        if (stream_next_bytes(s, raw, sizeof(raw)) == -1)
+            break;
+        mfm_decode_bytes(MFM_all, sizeof(sig), raw, sig);
+
+        /* First and last bytes of signature are static. */
+        if ((sig[0] != 0xa1) || (sig[9] != 0x00))
+            continue;
+
+        /* Sector must be followed by MFM-illegal flux transitions. */
+        nr_ones = bit_weight(&raw[0x18], 12);
+        if ((nr_ones > 12) && (nr_ones < 84))
+            continue;
+
+        memcpy(sigs[nr_sigs], sig, sizeof(sig));
+        valid_blocks |= 1u << sec;
+        nr_sigs++;
+    }
+
+    /* No matches at all? This was not an RNC-format track. */
+    if (nr_sigs == 0)
+        goto out;
+
+    /* Track validation: look for two matching sectors. */
+    for (i = found = 0; (i < nr_sigs) && !found; i++)
+        for (j = i+1; (j < nr_sigs) && !found; j++)
+            if (!memcmp(sigs[i], sigs[j], sizeof(sig)))
+                found = j;
+    /* If we find no matching pairs, we fail. */
+    if (!found) {
+        printf("*** T%u: RNC Hidden Sectors: Found no matching signatures "
+               "in %u sectors!\n", tracknr, nr_sigs);
+        goto out;
+    }
+    /* Otherwise, that's the signature we keep. */
+    memcpy(sig, sigs[found], sizeof(sig));
+
+    /* Count matches on our chosen signature. Warn on oddities. */
+    for (i = found = 0; i < nr_sigs; i++)
+        if (!memcmp(sigs[i], sig, sizeof(sig)))
+            found++;
+    if (found != nr_sigs)
+        printf("*** T%u: RNC Hidden Sectors: Found only %u matching "
+               "signatures out of %u\n", tracknr, found, nr_sigs);
+    if (nr_sigs != NR_SYNCS)
+        printf("*** T%u: RNC Hidden Sectors: Found only %u sectors "
+               "out of %u\n", tracknr, nr_sigs, NR_SYNCS);
+
+
+    /* Build the track decsriptor. */
+    init_track_info(ti, TRKTYP_rnc_hidden);
+    ti->len += sizeof(sig);
+    block = memalloc(ti->len);
+    memcpy(block, ablk, 512*11);
+    memcpy(&block[512*11], sig, sizeof(sig));
+
+out:
+    memfree(ablk);
+    return block;
+}
+
+static void rnc_hidden_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    uint8_t *dat = (uint8_t *)ti->dat + 512*11;
+    unsigned int sec, i;
+
+    handlers[TRKTYP_amigados]->read_mfm(d, tracknr, tbuf);
+
+    for (sec = 0; sec < NR_SYNCS; sec++) {
+        /* aaaa...aaaa */
+        for (i = 0; i < 16; i++)
+            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0);
+        /* sync */
+        tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, sync_list[sec]);
+        /* signature */
+        for (i = 0; i < 10; i++)
+            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, dat[i]);
+        /* raw zeroes */
+        for (i = 0; i < 64; i++)
+            tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 8, 0x00);
+    }
+}
+
+struct track_handler rnc_hidden_handler = {
+    .bytes_per_sector = 512,
+    .nr_sectors = 11,
+    .write_mfm = rnc_hidden_write_mfm,
+    .read_mfm = rnc_hidden_read_mfm
+};
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "Linux"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
