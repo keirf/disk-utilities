86f2b2c1c2582185985fe46bcfa2cfba054bc68a
Keir <keir.xen@gmail.com>
2012-07-09 19:27:08 +0100

    libdisk: Support B.A.T. by Ubisoft

---
+++ libdisk/bat.c
@@ -0,0 +1,95 @@
+/*
+ * disk/bat.c
+ * 
+ * Custom format as used on B.A.T. by Ubisoft.
+ * 
+ * Written in 2012 by Keir Fraser
+ * 
+ * RAW TRACK LAYOUT:
+ *  u16 0x8945
+ *  u32 data_even[0x628]
+ *  u32 csum_even
+ *  u32 data_odd[0x628]
+ *  u32 csum_odd
+ *  Checksum is sum of all decoded longs.
+ *  Track length is usual long (~105500 bitcells)
+ * 
+ * TRKTYP_bat data layout:
+ *  u8 sector_data[6304]
+ */
+
+#include <libdisk/util.h>
+#include "private.h"
+
+#include <arpa/inet.h>
+
+static void *bat_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+
+    while (stream_next_bit(s) != -1) {
+
+        uint32_t csum, dat[0x629*2];
+        unsigned int i;
+        char *block;
+
+        if ((uint16_t)s->word != 0x8945)
+            continue;
+
+        ti->data_bitoff = s->index_offset - 15;
+
+        if (stream_next_bytes(s, dat, sizeof(dat)) == -1)
+            break;
+        mfm_decode_bytes(MFM_even_odd, sizeof(dat)/2, dat, dat);
+
+        csum = tracknr ^ 1;
+        for (i = 0; i < 0x628; i++)
+            csum += ntohl(dat[i]);
+        if (csum != ntohl(dat[0x628]))
+            continue;
+
+        block = memalloc(ti->len);
+        memcpy(block, dat, ti->len);
+        ti->valid_sectors = (1u << ti->nr_sectors) - 1;
+        ti->total_bits = 105500;
+        return block;
+    }
+
+    return NULL;
+}
+
+static void bat_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    uint32_t csum, dat[0x629];
+    unsigned int i;
+
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, 0x8945);
+
+    memcpy(dat, ti->dat, ti->len);
+    csum = tracknr ^ 1;
+    for (i = 0; i < 0x628; i++)
+        csum += ntohl(dat[i]);
+    dat[0x628] = htonl(csum);
+
+    tbuf_bytes(tbuf, SPEED_AVG, MFM_even_odd, 0x629*4, dat);
+}
+
+struct track_handler bat_handler = {
+    .bytes_per_sector = 6304,
+    .nr_sectors = 1,
+    .write_mfm = bat_write_mfm,
+    .read_mfm = bat_read_mfm
+};
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "Linux"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
--- libdisk/longtrack.c
+++ libdisk/longtrack.c
@@ -5,22 +5,6 @@
  * 
  * Written in 2011 by Keir Fraser
  * 
- * TRKTYP_protec_longtrack: PROTEC protection track, used on many releases
- *  u16 0x4454
- *  u8 0x33 (encoded in-place, 1000+ times, to track gap)
- *  Track is checked to be >= 107200 bits long
- * 
- * TRKTYP_gremlin_longtrack: Lotus I/II
- *  u16 0x4124,0x4124
- *  Rest of track is (MFM-encoded) zeroes
- *  Track is checked to be >= 102400 bits long
- * 
- * TRKTYP_crystals_of_arborea_longtrack: Crystals Of Arborea
- *  u16 0xa144 :: sync
- *  u8[] "ROD0" (encoded MFM_all)
- *  Rest of track is (MFM-encoded) zeroes
- *  Track is checked to be >= 104128 bits long (track is ~110000 bits long)
- * 
  * TRKTYP_* data layout:
  *  No data (all track formats are fixed format with no key/real data)
  */
@@ -45,6 +29,13 @@ static int check_length(struct stream *s, unsigned int min_bits)
     stream_next_index(s);
     return (s->track_bitlen >= min_bits);
 }
+
+/*
+ * TRKTYP_protec_longtrack: PROTEC protection track, used on many releases
+ *  u16 0x4454
+ *  u8 0x33 (encoded in-place, 1000+ times, to track gap)
+ *  Track is checked to be >= 107200 bits long
+ */
 
 static void *protec_longtrack_write_mfm(
     struct disk *d, unsigned int tracknr, struct stream *s)
@@ -79,6 +70,13 @@ struct track_handler protec_longtrack_handler = {
     .read_mfm = protec_longtrack_read_mfm
 };
 
+/*
+ * TRKTYP_gremlin_longtrack: Lotus I/II
+ *  u16 0x4124,0x4124
+ *  Rest of track is (MFM-encoded) zeroes
+ *  Track is checked to be >= 102400 bits long
+ */
+
 static void *gremlin_longtrack_write_mfm(
     struct disk *d, unsigned int tracknr, struct stream *s)
 {
@@ -111,6 +109,14 @@ struct track_handler gremlin_longtrack_handler = {
     .write_mfm = gremlin_longtrack_write_mfm,
     .read_mfm = gremlin_longtrack_read_mfm
 };
+
+/*
+ * TRKTYP_crystals_of_arborea_longtrack: Crystals Of Arborea
+ *  u16 0xa144 :: sync
+ *  u8[] "ROD0" (encoded MFM_all)
+ *  Rest of track is (MFM-encoded) zeroes
+ *  Track is checked to be >= 104128 bits long (track is ~110000 bits long)
+ */
 
 static void *crystals_of_arborea_longtrack_write_mfm(
     struct disk *d, unsigned int tracknr, struct stream *s)
@@ -151,6 +157,46 @@ static void crystals_of_arborea_longtrack_read_mfm(
 struct track_handler crystals_of_arborea_longtrack_handler = {
     .write_mfm = crystals_of_arborea_longtrack_write_mfm,
     .read_mfm = crystals_of_arborea_longtrack_read_mfm
+};
+
+/*
+ * TRKTYP_bat_longtrack: B.A.T. by Ubisoft
+ *  u16 0x8945
+ *  Rest of track is (MFM-encoded) zeroes
+ *  Track is checked to be >= 109152 bits long (>= 3413 0xa...a longs)
+ */
+
+static void *bat_longtrack_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+
+    while (stream_next_bit(s) != -1) {
+        ti->data_bitoff = s->index_offset - 31;
+        if ((s->word != 0xaaaa8945) || !check_sequence(s, 6826, 0x00))
+            continue;
+        if (!check_length(s, 109500))
+            break;
+        ti->total_bits = 110000;
+        return memalloc(0);
+    }
+
+    return NULL;
+}
+
+static void bat_longtrack_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    unsigned int i;
+
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, 0x8945);
+    for (i = 0; i < 6840; i++)
+        tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0);
+}
+
+struct track_handler bat_longtrack_handler = {
+    .write_mfm = bat_longtrack_write_mfm,
+    .read_mfm = bat_longtrack_read_mfm
 };
 
 /*
