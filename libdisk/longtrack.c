2d366dcdb9dfb750449892054c2fa76c92c88c19
Keir <keir.xen@gmail.com>
2012-07-11 22:44:27 +0100

    libdisk: Support "Jumping Jack Son"

--- libdisk/longtrack.c
+++ libdisk/longtrack.c
@@ -35,6 +35,8 @@ static int check_length(struct stream *s, unsigned int min_bits)
  *  u16 0x4454
  *  u8 0x33 (encoded in-place, 1000+ times, to track gap)
  *  Track is checked to be >= 107200 bits long
+ *  Specifically, protection checks for >= 6700 mfm words gap between
+ *  successive sync marks. Track contents are not otherwise checked or tested.
  */
 
 static void *protec_longtrack_write_mfm(
@@ -74,7 +76,7 @@ struct track_handler protec_longtrack_handler = {
  * TRKTYP_gremlin_longtrack: Lotus I/II
  *  u16 0x4124,0x4124
  *  Rest of track is (MFM-encoded) zeroes, and/or unformatted garbage.
- *  The contents are never checked, only the sync mark is scanned for.
+ *  The contents are never checked, only successive sync marks are scanned for.
  *  Track length check is usually for >= 102400 bits, but there are variants
  *  (e.g., Strider II checks for normal length track!).
  */
@@ -125,6 +127,8 @@ struct track_handler gremlin_longtrack_handler = {
  *  u8[] "ROD0" (encoded MFM_all)
  *  Rest of track is (MFM-encoded) zeroes
  *  Track is checked to be >= 104128 bits long (track is ~110000 bits long)
+ *  Specifically, protection checks for > 6500 0xaaaa/0x5555 mfm words
+ *  starting 12 bytes into the DMA buffer (i.e., 12 bytes after the sync)
  */
 
 static void *crystals_of_arborea_longtrack_write_mfm(
@@ -169,10 +173,56 @@ struct track_handler crystals_of_arborea_longtrack_handler = {
 };
 
 /*
+ * TRKTYP_jumping_jack_son_longtrack: Jumping Jack Son
+ *  u16 0xa144 :: sync
+ *  Rest of track is (MFM-encoded) zeroes
+ *  Track is checked to be >= 104160 bits long (track is ~105500 bits long)
+ *  Specifically, protection checks for > 13020 0xaa mfm bytes, starting from
+ *  the first 0xaa byte in the DMA buffer (i.e., first 0xaa following sync).
+ */
+
+static void *jumping_jack_son_longtrack_write_mfm(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+
+    while (stream_next_bit(s) != -1) {
+        ti->data_bitoff = s->index_offset - 15;
+        if ((uint16_t)s->word != 0xa144)
+            continue;
+        if (!check_sequence(s, 6510, 0x00))
+            continue;
+        if (!check_length(s, 104160))
+            break;
+        ti->total_bits = 105500;
+        return memalloc(0);
+    }
+
+    return NULL;
+}
+
+static void jumping_jack_son_longtrack_read_mfm(
+    struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
+{
+    unsigned int i;
+
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 16, 0xa144);
+    for (i = 0; i < 6550; i++)
+        tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0);
+}
+
+struct track_handler jumping_jack_son_longtrack_handler = {
+    .write_mfm = jumping_jack_son_longtrack_write_mfm,
+    .read_mfm = jumping_jack_son_longtrack_read_mfm
+};
+
+/*
  * TRKTYP_bat_longtrack: B.A.T. by Ubisoft
  *  u16 0x8945
  *  Rest of track is (MFM-encoded) zeroes
  *  Track is checked to be >= 109152 bits long (>= 3413 0xa...a longs)
+ *  Specifically, protection checks for >= 3412 0xaaaaaaaa mfm longwords
+ *  starting 4 bytes into the DMA buffer (i.e., 4 bytes after the sync)
  */
 
 static void *bat_longtrack_write_mfm(
