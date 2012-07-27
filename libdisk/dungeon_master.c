7f7cb691e82d4f9aa55d1181a5e035b2a27e5548
Keir <keir.xen@gmail.com>
2012-07-27 21:47:07 +0100

    libdisk: Improve Dungeon Master decode. All done except *generating* weak bits. ;)

--- libdisk/dungeon_master.c
+++ libdisk/dungeon_master.c
@@ -3,6 +3,14 @@
  * 
  * An Atari ST (i.e., IBM-compatible) MFM track with weak bits in sector 1.
  * 
+ * The protection relies on an ambiguous flux transition at the edge of the
+ * FDC's inspection window, which may be interpreted as clock or as data.
+ * Thus the MSB of each byte in the weak area is randomly read as 0 or 1.
+ * 
+ * Note that this relies on fairly authentic PLL behaviour in the flux
+ * decoder, to respond slowly to 'out of sync' pulses. Else we can lose sync
+ * with the bit stream.
+ * 
  * Notes on IBM-compatible MFM data format:
  * ----------------------------------------
  * Supported by uPD765A, Intel 8272, and many other FDC chips, as used in
@@ -47,6 +55,8 @@ static void *dungeon_master_weak_write_mfm(
 
         uint32_t sz, idx_off = s->index_offset - 31;
         uint8_t dat[2*514], cyl, head, sec, no;
+        uint16_t crc;
+        unsigned int i;
 
         /* IDAM */
         if (s->word != 0x44894489)
@@ -86,25 +96,45 @@ static void *dungeon_master_weak_write_mfm(
             break;
         if (s->word != 0x44895545)
             continue;
+        crc = s->crc16_ccitt;
+
+        /*
+         * Sector 0 weak-bit protection relies on authentic behaviour of FDC 
+         * PLL to respond slowly to marginal bits at edge of inspection window.
+         */
+        stream_authentic_pll(s, (sec == 0));
         if (stream_next_bytes(s, dat, sizeof(dat)) == -1)
             break;
-        if ((sec == 0) != (s->crc16_ccitt != 0))
+        stream_authentic_pll(s, 0);
+        mfm_decode_bytes(MFM_all, 514, dat, dat);
+
+        if (sec == 0) {
+            /*
+             * Check each flakey byte is read as 0x68 or 0xE8. Rewrite as
+             * originally mastered (always 0x68, with timing variation).
+             * Any mismatching bytes will cause us to fail the CRC check.
+             */
+            for (i = 20; i < 509; i++)
+                if ((dat[i]&0x7f) == 0x68)
+                    dat[i] = 0x68;
+            /* Re-compute the CRC on fixed-up data. */
+            s->crc16_ccitt = crc16_ccitt(dat, 514, crc);
+        }
+
+        if (s->crc16_ccitt != 0)
             continue;
 
-        mfm_decode_bytes(MFM_all, sz, dat, dat);
         memcpy(&block[sec*512], dat, 512);
         valid_blocks |= 1u << sec;
         if (sec == 0)
             ti->data_bitoff = idx_off;
     }
 
-    if (valid_blocks == 0) {
+    /* Must have found valid sector 0 */
+    if (!(valid_blocks & 1u)) {
         memfree(block);
         return NULL;
     }
-
-    if (!(valid_blocks & 1u))
-        ti->data_bitoff = 1100;
 
     ti->valid_sectors = valid_blocks;
 
@@ -138,10 +168,7 @@ static void dungeon_master_weak_read_mfm(
         tbuf_start_crc(tbuf);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44894489);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44895545);
-        if (sec == 0)
-            tbuf_weak(tbuf, SPEED_AVG, 512*8);
-        else
-            tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 512, &dat[sec*512]);
+        tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 512, &dat[sec*512]);
         tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
         for (i = 0; i < 40; i++)
             tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x4e);
