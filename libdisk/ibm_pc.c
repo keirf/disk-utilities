5b2585950c0cc08480584f24d6569d536d5c18e2
Keir <keir.xen@gmail.com>
2012-07-31 13:12:03 +0100

    libdisk: New formats "ibm_pc_dd" and "ibm_pc_hd"

--- libdisk/ibm_pc.c
+++ libdisk/ibm_pc.c
@@ -1,17 +1,28 @@
 /*
- * disk/dungeon_master.c
+ * disk/ibm_pc.c
  * 
- * An Atari ST (i.e., IBM-compatible) MFM track with weak bits in sector 1.
+ * 9 (DD) or 18 (HD) 512-byte sectors in IBM System/34 format.
  * 
- * The protection relies on an ambiguous flux transition at the edge of the
- * FDC's inspection window, which may be interpreted as clock or as data.
- * Thus the MSB of each byte in the weak area is randomly read as 0 or 1.
+ * Notes on IBM-compatible MFM data format:
+ * ----------------------------------------
+ * Supported by uPD765A, Intel 8272, and many other FDC chips, as used in
+ * pretty much every home computer (except Amiga and C64!).
  * 
- * Note that this relies on fairly authentic PLL behaviour in the flux
- * decoder, to respond slowly to 'out of sync' pulses. Else we can lose sync
- * with the bit stream.
+ * One of the more useful references:
+ *  "uPD765A/7265 Single/Double Density Floppy Disk Controllers",
+ *  NEC Electronics Inc.
  * 
- * See ibm_pc.c for technical details on the IBM-compatible MFM data format.
+ * Index Address Mark (IAM):
+ *      0xc2c2c2fc
+ * ID Address Mark (IDAM):
+ *      0xa1a1a1fe, <cyl>, <hd> <sec>, <sz>, <crc16_ccitt>
+ * Data Address Mark (DAM):
+ *      0xa1a1a1fb, <N bytes data>, <crc16_ccitt> [N = 128 << sz]
+ * Deleted Data Address Mark (DDAM):
+ *      As DAM, but identifier 0xfb -> 0xf8
+ * 
+ * NB. In above, 0xc2 and 0xa1 are sync marks which have one of their clock
+ *     bits forced to zero. Hence 0xc2 -> 0x5224; 0xa1 -> 0x4489.
  * 
  * Written in 2012 by Keir Fraser
  */
@@ -21,45 +32,56 @@
 
 #include <arpa/inet.h>
 
-static void *dungeon_master_weak_write_mfm(
+static void *ibm_pc_write_mfm(
     struct disk *d, unsigned int tracknr, struct stream *s)
 {
     struct track_info *ti = &d->di->track[tracknr];
     char *block = memalloc(ti->bytes_per_sector * ti->nr_sectors);
     unsigned int valid_blocks = 0;
 
-    /* Fill value for all sectors seems to be 0xe5. */
-    memset(block, 0xe5, ti->bytes_per_sector * ti->nr_sectors);
+    /* IAM */
+    while (stream_next_bit(s) != -1) {
+        if (s->word != 0x52245224)
+            continue;
+        if (stream_next_bits(s, 32) == -1)
+            goto out;
+        if (s->word == 0x52245552) {
+            printf("*** T%u: IAM detected - not yet handled!\n", tracknr);
+            goto out;
+        }
+    }
+
+    ti->data_bitoff = s->index_offset - 63;
+    stream_reset(s);
 
     while ((stream_next_bit(s) != -1) &&
            (valid_blocks != ((1u<<ti->nr_sectors)-1))) {
 
         uint32_t sz, idx_off = s->index_offset - 31;
         uint8_t dat[2*514], cyl, head, sec, no;
-        uint16_t crc;
-        unsigned int i;
 
         /* IDAM */
         if (s->word != 0x44894489)
             continue;
         stream_start_crc(s);
         if (stream_next_bits(s, 32) == -1)
-            break;
+            goto out;
         if (s->word != 0x44895554)
             continue;
 
         if (stream_next_bits(s, 32) == -1)
-            break;
+            goto out;
         cyl = mfm_decode_bits(MFM_all, s->word >> 16);
         head = mfm_decode_bits(MFM_all, s->word);
         if (stream_next_bits(s, 32) == -1)
-            break;
+            goto out;
         sec = mfm_decode_bits(MFM_all, s->word >> 16);
         no = mfm_decode_bits(MFM_all, s->word);
         if (stream_next_bits(s, 32) == -1)
-            break;
+            goto out;
         sz = 128 << no;
-        if ((cyl != 0) || (head != 1) || (sz != 512) || (s->crc16_ccitt != 0))
+        if ((cyl != (tracknr/2)) || (head != (tracknr&1)) ||
+            (sz != 512) || (s->crc16_ccitt != 0))
             continue;
 
         sec--;
@@ -74,64 +96,53 @@ static void *dungeon_master_weak_write_mfm(
             continue;
         stream_start_crc(s);
         if (stream_next_bits(s, 32) == -1)
-            break;
+            goto out;
         if (s->word != 0x44895545)
             continue;
-        crc = s->crc16_ccitt;
-
-        /*
-         * Sector 0 weak-bit protection relies on authentic behaviour of FDC 
-         * PLL to respond slowly to marginal bits at edge of inspection window.
-         */
-        stream_authentic_pll(s, (sec == 0));
         if (stream_next_bytes(s, dat, sizeof(dat)) == -1)
-            break;
-        stream_authentic_pll(s, 0);
-        mfm_decode_bytes(MFM_all, 514, dat, dat);
-
-        if (sec == 0) {
-            /*
-             * Check each flakey byte is read as 0x68 or 0xE8. Rewrite as
-             * originally mastered (always 0x68, with timing variation).
-             * Any mismatching bytes will cause us to fail the CRC check.
-             */
-            for (i = 20; i < 509; i++)
-                if ((dat[i]&0x7f) == 0x68)
-                    dat[i] = 0x68;
-            /* Re-compute the CRC on fixed-up data. */
-            s->crc16_ccitt = crc16_ccitt(dat, 514, crc);
-        }
-
+            goto out;
         if (s->crc16_ccitt != 0)
             continue;
 
+        mfm_decode_bytes(MFM_all, 512, dat, dat);
         memcpy(&block[sec*512], dat, 512);
         valid_blocks |= 1u << sec;
         if (sec == 0)
             ti->data_bitoff = idx_off;
     }
 
-    /* Must have found valid sector 0 */
-    if (!(valid_blocks & 1u)) {
+out:
+    if (!valid_blocks) {
         memfree(block);
         return NULL;
     }
 
+    ti->data_bitoff = 2240;
     ti->valid_sectors = valid_blocks;
 
     return block;
 }
 
-static void dungeon_master_weak_read_mfm(
+static void ibm_pc_read_mfm(
     struct disk *d, unsigned int tracknr, struct track_buffer *tbuf)
 {
     struct track_info *ti = &d->di->track[tracknr];
     uint8_t *dat = (uint8_t *)ti->dat;
-    uint8_t cyl = 0, hd = 1, no = 2;
-    unsigned int sec, i;
+    uint8_t cyl = tracknr/2, hd = tracknr&1, no = 2;
+    unsigned int sec, i, gap4;
+
+    gap4 = (ti->type == TRKTYP_ibm_pc_dd) ? 80 : 108;
+
+#if 0
+    /* IAM */
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x52245224);
+    tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x52245552);
+#endif
 
     for (sec = 0; sec < ti->nr_sectors; sec++) {
         /* IDAM */
+        for (i = 0; i < 12; i++)
+            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x00);
         tbuf_start_crc(tbuf);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44894489);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44895554);
@@ -142,40 +153,39 @@ static void dungeon_master_weak_read_mfm(
         tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
         for (i = 0; i < 22; i++)
             tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x4e);
-        for (i = 0; i < 12; i++)
-            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x00);
 
         /* DAM */
+        for (i = 0; i < 12; i++)
+            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x00);
         tbuf_start_crc(tbuf);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44894489);
         tbuf_bits(tbuf, SPEED_AVG, MFM_raw, 32, 0x44895545);
-        if (sec == 0) {
-            uint16_t crc = crc16_ccitt(dat, 512, tbuf->crc16_ccitt);
-            static unsigned int seed = 0;
-            tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 32, &dat[0]);
-            /* Protection sector: randomise MSB of each byte in weak area. */
-            for (i = 0; i < 512-64; i++)
-                tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8,
-                          (rand_r(&seed) & 1) ? 0x68 : 0xe8);
-            tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 32, &dat[512-32]);
-            /* CRC is generated pre-randomisation. Restore it now. */
-            tbuf->crc16_ccitt = crc;
-        } else {
-            tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 512, &dat[sec*512]);
-        }
+        tbuf_bytes(tbuf, SPEED_AVG, MFM_all, 512, &dat[sec*512]);
         tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
-        for (i = 0; i < 40; i++)
+        for (i = 0; i < gap4; i++)
             tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x4e);
-        for (i = 0; i < 12; i++)
-            tbuf_bits(tbuf, SPEED_AVG, MFM_all, 8, 0x00);
     }
+
+    /*
+     * NB. Proper track gap should be 0x4e recurring up to the index mark.
+     * Then write splice. Then ~140*0x4e, leading into 12*0x00.
+     */
 }
 
-struct track_handler dungeon_master_weak_handler = {
+struct track_handler ibm_pc_dd_handler = {
+    .density = TRKDEN_mfm_double,
     .bytes_per_sector = 512,
-    .nr_sectors = 10,
-    .write_mfm = dungeon_master_weak_write_mfm,
-    .read_mfm = dungeon_master_weak_read_mfm
+    .nr_sectors = 9,
+    .write_mfm = ibm_pc_write_mfm,
+    .read_mfm = ibm_pc_read_mfm
+};
+
+struct track_handler ibm_pc_hd_handler = {
+    .density = TRKDEN_mfm_high,
+    .bytes_per_sector = 512,
+    .nr_sectors = 18,
+    .write_mfm = ibm_pc_write_mfm,
+    .read_mfm = ibm_pc_read_mfm
 };
 
 /*
