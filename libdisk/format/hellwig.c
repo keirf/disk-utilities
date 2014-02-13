21525c3ed4a62ab5bbfd29b68c8ed0a73e4bbbbf
Keir <keir.xen@gmail.com>
2014-02-13 14:38:21 +0000

    libdisk: Simplify Hellwig track decoders

--- libdisk/format/hellwig.c
+++ libdisk/format/hellwig.c
@@ -10,25 +10,21 @@
  * Written in 2014 by Keith Krellwitz
  *
  * RAW TRACK LAYOUT:
- *  u16 0x4489  :: Sync
- *  u16 0x4489 :: Extra sync in Format B
- *  u16 0
+ *  u16 0x4489  :: Sync (possibly x2)
+ *  u8  0
  *  u32 dat[5120/4]
  *  u32 dat[6200/4] :: apprentice
  *  u32 checksum
  *
- * TRKTYP_hellwig data layout:
- *  u8 sector_data[5120]
- *
- * TRKTYP_dangerfreak data layout:
- *  u8 sector_data[5120]
- *
- * TRKTYP_apprentice data layout:
- *  u8 sector_data[6200]
+ * TRKTYP_* data layout:
+ *  u8 sector_data[bytes_per_sector]
+ *  u8 checksum_type | (two_sync << 4)
  */
 
 #include <libdisk/util.h>
 #include "../private.h"
+
+enum checksum_type { CSUM_zeroes, CSUM_ones, CSUM_data };
 
 static void *hellwig_write_raw(
     struct disk *d, unsigned int tracknr, struct stream *s)
@@ -38,25 +34,22 @@ static void *hellwig_write_raw(
     while (stream_next_bit(s) != -1) {
 
         uint32_t raw[2], dat[ti->bytes_per_sector/4], csum, sum;
-        unsigned int i;
+        enum checksum_type checksum_type;
+        unsigned int i, two_sync;
         char *block;
 
         if ((uint16_t)s->word != 0x4489)
             continue;
 
+        ti->data_bitoff = s->index_offset - 15;
+
         if (stream_next_bits(s, 16) == -1)
             goto fail;
-        if (s->word == 0x44894489) {
-            if (stream_next_bits(s, 16) == -1)
-                goto fail;
-            if (s->word != 0x44892aaa)
-                continue;
-            ti->data_bitoff = s->index_offset - 47;
-        } else if (s->word == 0x44892aaa) {
-            ti->data_bitoff = s->index_offset - 31;
-        } else {
+        two_sync = (s->word == 0x44894489);
+        if (two_sync && (stream_next_bits(s, 16) == -1))
+            goto fail;
+        if (s->word != 0x44892aaa)
             continue;
-        }
 
         for (i = sum = 0; i < ARRAY_SIZE(dat); i++) {
             if (stream_next_bytes(s, raw, 8) == -1)
@@ -68,12 +61,22 @@ static void *hellwig_write_raw(
         if (stream_next_bytes(s, raw, 8) == -1)
             goto fail;
         mfm_decode_bytes(bc_mfm_even_odd, 4, raw, &csum);
+        csum = be32toh(csum);
 
-        if ((be32toh(csum) != sum) && (csum != 0) && (csum != 0xffffffff))
-            continue;
+        if (csum == 0) {
+            checksum_type = CSUM_zeroes;
+        } else if (csum == 0xffffffff) {
+            checksum_type = CSUM_ones;
+        } else {
+            checksum_type = CSUM_data;
+            if (csum != sum)
+                continue;
+        }
 
+        ti->len += 1;
         block = memalloc(ti->len);
-        memcpy(block, dat, ti->len);
+        memcpy(block, dat, ti->len-1);
+        block[ti->len-1] = checksum_type | (two_sync << 4);
         ti->total_bits = 102000;
         set_all_sectors_valid(ti);
         return block;
@@ -88,43 +91,25 @@ static void hellwig_read_raw(
 {
     struct track_info *ti = &d->di->track[tracknr];
     uint32_t csum, *dat = (uint32_t *)ti->dat;
-    unsigned int i;
+    enum checksum_type checksum_type = ti->dat[ti->len-1] & 0xf;
+    unsigned int i, two_sync = ti->dat[ti->len-1] >> 4;
 
-    if (ti->type == TRKTYP_hellwig_b)
+    if (two_sync)
         tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, 0x4489);
     tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x44892aaa);
 
-    for (i = csum = 0; i < ti->len/4; i++) {
+    for (i = csum = 0; i < ti->bytes_per_sector/4; i++) {
         tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, be32toh(dat[i]));
         csum += be32toh(dat[i]);
     }
 
-    if (ti->type == TRKTYP_dangerfreak) {
-        if (tracknr == 7 || tracknr == 9)
-            csum = 0xffffffff;
-    } else if (ti->type == TRKTYP_hellwig_a) {
-        if (tracknr == 1)
-            csum = (csum == 0x9ec821a) ? 0 : 0xffffffff;
-    }
-
-    tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, csum);
+    tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32,
+              (checksum_type == CSUM_zeroes) ? 0x00000000
+              : (checksum_type == CSUM_ones) ? 0xffffffff
+              : csum);
 }
 
-struct track_handler hellwig_a_handler = {
-    .bytes_per_sector = 5120,
-    .nr_sectors = 1,
-    .write_raw = hellwig_write_raw,
-    .read_raw = hellwig_read_raw
-};
-
-struct track_handler hellwig_b_handler = {
-    .bytes_per_sector = 5120,
-    .nr_sectors = 1,
-    .write_raw = hellwig_write_raw,
-    .read_raw = hellwig_read_raw
-};
-
-struct track_handler dangerfreak_handler = {
+struct track_handler hellwig_handler = {
     .bytes_per_sector = 5120,
     .nr_sectors = 1,
     .write_raw = hellwig_write_raw,
