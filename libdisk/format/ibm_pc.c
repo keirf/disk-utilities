d6d9daace3789a29858bab0b3fc9d9da8a3d6eb6
Keir <keir.xen@gmail.com>
2014-07-30 17:19:36 +0100

    libdisk: New generic IBM-MFM format handlers: ibm_mfm_{dd,hd,ed}

--- libdisk/format/ibm_pc.c
+++ libdisk/format/ibm_pc.c
@@ -86,7 +86,7 @@ int ibm_scan_dam(struct stream *s)
     return ibm_scan_mark(s, 0x5545, 1000);
 }
 
-static void *ibm_pc_write_raw(
+static void *ibm_img_write_raw(
     struct disk *d, unsigned int tracknr, struct stream *s)
 {
     struct track_info *ti = &d->di->track[tracknr];
@@ -169,7 +169,7 @@ static void *ibm_pc_write_raw(
     return block;
 }
 
-static void ibm_pc_read_raw(
+static void ibm_img_read_raw(
     struct disk *d, unsigned int tracknr, struct tbuf *tbuf)
 {
     struct track_info *ti = &d->di->track[tracknr];
@@ -229,7 +229,7 @@ static void ibm_pc_read_raw(
      * Then write splice. Then ~140*0x4e, leading into 12*0x00. */
 }
 
-void *ibm_pc_write_sectors(
+void *ibm_img_write_sectors(
     struct disk *d, unsigned int tracknr, struct track_sectors *sectors)
 {
     struct track_info *ti = &d->di->track[tracknr];
@@ -251,7 +251,7 @@ void *ibm_pc_write_sectors(
     return block;
 }
 
-void ibm_pc_read_sectors(
+void ibm_img_read_sectors(
     struct disk *d, unsigned int tracknr, struct track_sectors *sectors)
 {
     struct track_info *ti = &d->di->track[tracknr];
@@ -267,10 +267,10 @@ struct track_handler ibm_pc_dd_handler = {
     .density = trkden_double,
     .bytes_per_sector = 512,
     .nr_sectors = 9,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -281,10 +281,10 @@ struct track_handler ibm_pc_dd_10sec_handler = {
     .density = trkden_double,
     .bytes_per_sector = 512,
     .nr_sectors = 10,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -295,10 +295,10 @@ struct track_handler ibm_dd_10sec_handler = {
     .density = trkden_double,
     .bytes_per_sector = 512,
     .nr_sectors = 10,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 0
     }
@@ -309,10 +309,10 @@ struct track_handler ibm_pc_hd_5_25_handler = {
     .density = trkden_high,
     .bytes_per_sector = 512,
     .nr_sectors = 15,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -322,10 +322,10 @@ struct track_handler ibm_pc_hd_handler = {
     .density = trkden_high,
     .bytes_per_sector = 512,
     .nr_sectors = 18,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -335,10 +335,10 @@ struct track_handler ibm_pc_ed_handler = {
     .density = trkden_extra,
     .bytes_per_sector = 512,
     .nr_sectors = 36,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -349,10 +349,10 @@ struct track_handler siemens_isdx_hd_handler = {
     .density = trkden_high,
     .bytes_per_sector = 256,
     .nr_sectors = 32,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -364,10 +364,10 @@ struct track_handler microsoft_dmf_hd_handler = {
     .density = trkden_high,
     .bytes_per_sector = 512,
     .nr_sectors = 21,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -380,10 +380,10 @@ struct track_handler trace_traceback_hd_handler = {
     .density = trkden_high,
     .bytes_per_sector = 2048,
     .nr_sectors = 1,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 1
     }
@@ -397,10 +397,10 @@ struct track_handler acorn_adfs_s_m_l_handler = {
     .density = trkden_double,
     .bytes_per_sector = 256,
     .nr_sectors = 16,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 0
     }
@@ -411,10 +411,10 @@ struct track_handler acorn_adfs_d_e_handler = {
     .density = trkden_double,
     .bytes_per_sector = 1024,
     .nr_sectors = 5,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 0
     }
@@ -425,10 +425,10 @@ struct track_handler acorn_adfs_f_handler = {
     .density = trkden_high,
     .bytes_per_sector = 1024,
     .nr_sectors = 10,
-    .write_raw = ibm_pc_write_raw,
-    .read_raw = ibm_pc_read_raw,
-    .write_sectors = ibm_pc_write_sectors,
-    .read_sectors = ibm_pc_read_sectors,
+    .write_raw = ibm_img_write_raw,
+    .read_raw = ibm_img_read_raw,
+    .write_sectors = ibm_img_write_sectors,
+    .read_sectors = ibm_img_read_sectors,
     .extra_data = & (struct ibm_extra_data) {
         .sector_base = 0
     }
@@ -438,6 +438,219 @@ struct track_handler acorn_adfs_f_handler = {
  * FM decode support:
  *   DFS 40-track - 40tk DS 10/256  200K  FM/SD
  *   DFS 80-track - 80th DS 10/256  400K  FM/SD */
+
+struct ibm_sector {
+    struct ibm_idam idam;
+    uint8_t dat[0];
+};
+
+struct ibm_track {
+    uint8_t has_iam;
+    uint8_t gap4;
+    struct ibm_sector secs[0];
+};
+
+struct ibm_psector {
+    struct ibm_psector *next;
+    int offset;
+    struct ibm_sector s;
+};
+
+static void *ibm_mfm_write_raw(
+    struct disk *d, unsigned int tracknr, struct stream *s)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    struct ibm_psector *ibm_secs, *new_sec, *cur_sec, *next_sec, **pprev_sec;
+    struct ibm_track *ibm_track = NULL;
+    unsigned int dat_bytes = 0, total_distance = 0, nr_blocks = 0;
+    unsigned int sec_sz, gap4;
+    bool_t iam = 0;
+
+    /* IAM */
+    while (!iam && (stream_next_bit(s) != -1)) {
+        if (s->word != 0x52245224)
+            continue;
+        if (stream_next_bits(s, 32) == -1)
+            break;
+        iam = (s->word == 0x52245552);
+    }
+
+    stream_reset(s);
+
+    ibm_secs = NULL;
+
+    while (stream_next_bit(s) != -1) {
+
+        int idx_off;
+        uint8_t dat[2*16384];
+        struct ibm_idam idam;
+
+        /* IDAM */
+        if (((idx_off = ibm_scan_idam(s, &idam)) < 0) || s->crc16_ccitt)
+            continue;
+
+        if (idam.no > 7) {
+            trk_warn(ti, tracknr, "Unexpected IDAM no=%02x", idam.no);
+            continue;
+        }
+
+        sec_sz = 128 << idam.no;
+
+        /* Find correct place for this sector in our linked list of sectors 
+         * that we have decoded so far. */
+        pprev_sec = &ibm_secs;
+        cur_sec = *pprev_sec;
+        while (cur_sec && ((idx_off - cur_sec->offset) > 1000)) {
+            pprev_sec = &cur_sec->next;
+            cur_sec = *pprev_sec;
+        }
+
+        /* If this sector's start is within 1000 bits of one we already decoded
+         * then it is the same sector: we decoded it already on an earlier 
+         * revolution and can skip it this time round. */
+        if (cur_sec && (abs(idx_off - cur_sec->offset) < 1000))
+            continue;
+
+        /* DAM */
+        if (((idx_off = ibm_scan_dam(s)) < 0) ||
+            (stream_next_bytes(s, dat, 2*sec_sz) == -1) ||
+            (stream_next_bits(s, 32) == -1) || s->crc16_ccitt)
+            continue;
+
+        mfm_decode_bytes(bc_mfm, sec_sz, dat, dat);
+
+        new_sec = memalloc(sizeof(*new_sec) + sec_sz);
+        new_sec->offset = idx_off;
+        memcpy(&new_sec->s.dat[0], dat, sec_sz);
+        memcpy(&new_sec->s.idam, &idam, sizeof(idam));
+        new_sec->next = *pprev_sec;
+        *pprev_sec = new_sec;
+    }
+
+    for (cur_sec = ibm_secs; cur_sec; cur_sec = cur_sec->next) {
+        int distance, cur_size;
+        next_sec = cur_sec->next ?: ibm_secs;
+        distance = next_sec->offset - cur_sec->offset;
+        if (distance <= 0)
+            distance += s->track_bitlen;
+        sec_sz = 128 << cur_sec->s.idam.no;
+        cur_size = 62 + sec_sz;
+        if ((distance -= cur_size * 16) < 0) {
+            trk_warn(ti, tracknr, "Overlapping sectors");
+            goto out;
+        }
+        total_distance += distance;
+        nr_blocks++;
+        dat_bytes += sec_sz;
+    }
+
+    if (nr_blocks == 0)
+        goto out;
+
+    gap4 = iam
+        ? (total_distance - 16*16) / ((nr_blocks+1) * 16)
+        : total_distance / (nr_blocks * 16);
+    gap4 = (gap4 > 108+2) ? 108
+        : (gap4 > 80+2) ? 80
+        : (gap4 > 40+2) ? 40
+        : 20;
+
+    ti->data_bitoff = (iam ? 80 : 140) * 16;
+    ti->nr_sectors = nr_blocks;
+    set_all_sectors_valid(ti);
+
+    ibm_track = memalloc(sizeof(struct ibm_track)
+                         + nr_blocks * sizeof(struct ibm_sector)
+                         + dat_bytes);
+
+    ibm_track->has_iam = iam ? 1 : 0;
+    ibm_track->gap4 = gap4;
+
+    ti->len = sizeof(struct ibm_track);
+    for (cur_sec = ibm_secs; cur_sec; cur_sec = cur_sec->next) {
+        sec_sz = 128 << cur_sec->s.idam.no;
+        memcpy((char *)ibm_track + ti->len,
+               &cur_sec->s, sizeof(struct ibm_sector) + sec_sz);
+        ti->len += sizeof(struct ibm_sector) + sec_sz;
+    }
+
+out:
+    return ibm_track;
+}
+
+static void ibm_mfm_read_raw(
+    struct disk *d, unsigned int tracknr, struct tbuf *tbuf)
+{
+    struct track_info *ti = &d->di->track[tracknr];
+    struct ibm_track *ibm_track = (struct ibm_track *)ti->dat;
+    struct ibm_sector *cur_sec;
+    unsigned int sec, i, sec_sz;
+
+    /* IAM */
+    if (ibm_track->has_iam) {
+        for (i = 0; i < 12; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x00);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x52245224);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x52245552);
+        for (i = 0; i < ibm_track->gap4; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x4e);
+    }
+
+    cur_sec = ibm_track->secs;
+    for (sec = 0; sec < ti->nr_sectors; sec++) {
+
+        sec_sz = 128 << cur_sec->idam.no;
+
+        /* IDAM */
+        for (i = 0; i < 12; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x00);
+        tbuf_start_crc(tbuf);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x44894489);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x44895554);
+        tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, cur_sec->idam.cyl);
+        tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, cur_sec->idam.head);
+        tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, cur_sec->idam.sec);
+        tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, cur_sec->idam.no);
+        tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
+        for (i = 0; i < 22; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x4e);
+
+        /* DAM */
+        for (i = 0; i < 12; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x00);
+        tbuf_start_crc(tbuf);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x44894489);
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x44895545);
+        tbuf_bytes(tbuf, SPEED_AVG, bc_mfm, sec_sz, cur_sec->dat);
+        tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
+        for (i = 0; i < ibm_track->gap4; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, 0x4e);
+
+        cur_sec = (struct ibm_sector *)
+            ((char *)cur_sec + sizeof(struct ibm_sector) + sec_sz);
+    }
+
+    /* NB. Proper track gap should be 0x4e recurring up to the index mark.
+     * Then write splice. Then ~140*0x4e, leading into 12*0x00. */
+}
+
+struct track_handler ibm_mfm_dd_handler = {
+    .density = trkden_double,
+    .write_raw = ibm_mfm_write_raw,
+    .read_raw = ibm_mfm_read_raw
+};
+
+struct track_handler ibm_mfm_hd_handler = {
+    .density = trkden_high,
+    .write_raw = ibm_mfm_write_raw,
+    .read_raw = ibm_mfm_read_raw
+};
+
+struct track_handler ibm_mfm_ed_handler = {
+    .density = trkden_extra,
+    .write_raw = ibm_mfm_write_raw,
+    .read_raw = ibm_mfm_read_raw
+};
 
 /*
  * Local variables:
