48b5562c591d55b67d6f3ce8ae05c2ff39200c55
Keir Fraser <keir.xen@gmail.com>
2015-08-13 08:09:22 +0100

    libdisk: Support Sextett Disk 2

--- libdisk/format/amiga/sextett.c
+++ libdisk/format/amiga/sextett.c
@@ -3,6 +3,8 @@
  * 
  * Sextett compilation by Kingsoft
  *
+ * DISK 1:
+ * -------
  * Track 158:
  *  u16 0x92459245
  *  u8 key[3] :: raw bytes
@@ -13,7 +15,11 @@
  * simultaneously starting an unsynchronised read DMA. The key from T158 is
  * used as an offset into the read buffer, which must contain a 0x9245 sync
  * word at that offset.
- * We simulate this by filling tracks 160 and 161 with 0x9245.
+ * We simulate this by filling track 161 with 0x9245.
+ * 
+ * DISK 2:
+ * -------
+ * Track 161 is entirely filled with 0xaa bytes (MFM 0x4444).
  */
 
 #include <libdisk/util.h>
@@ -25,13 +31,18 @@ static void *sextett_protection_write_raw(
     struct track_info *ti = &d->di->track[tracknr];
     uint32_t *block;
 
-    /* Tracks 159-161: no data, all same data_bitoff (==0) */
-    if ((tracknr >= 159) && (tracknr <= 161)) {
+    /* Tracks 159 & 161: no data, all same data_bitoff (==0) */
+    if (tracknr == 159) {
+        /* Track 159 is only a protection track on Disk 1. */
         struct track_info *t158 = &d->di->track[158];
         return (t158->type == TRKTYP_sextett_protection) ? memalloc(0) : NULL;
+    } else if (tracknr == 161) {
+        /* Track 161: Protection on all disks. */
+        ti->total_bits &= ~15;
+        return memalloc(0);
     }
 
-    /* Track 158: find the key */
+    /* Disk 1, Track 158: find the key */
     while (stream_next_bit(s) != -1) {
         if (s->word != 0x92459245)
             continue;
@@ -54,16 +65,21 @@ static void sextett_protection_read_raw(
     unsigned int i;
 
     if (tracknr == 158) {
-        /* Key track */
+        /* Disk 1: Key track */
         tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x92459245);
         tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, be32toh(*dat));
     } else if (tracknr == 159) {
-        /* Sync track */
+        /* Disk 1: Sync track */
         tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x92459245);
+    } else if (d->di->track[158].type == TRKTYP_sextett_protection) {
+        /* Disk 1: Landing track */
+        for (i = 0; i < ti->total_bits/16; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, 0x9245);
     } else {
-        /* Landing track */
-        for (i = 0; i < 3000; i++)
-            tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x92459245);
+        /* Disk 2: Pattern track */
+        for (i = 0; i < ti->total_bits/8-1; i++)
+            tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 4, 0xa);
+        tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 4, 0x9); /* discontinuity */
     }
 }
 
