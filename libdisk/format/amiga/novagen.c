2de61887a8c83020a6f43a7acbd55f2caed3b9e0
Keir Fraser <keir.xen@gmail.com>
2020-02-14 08:12:48 +0000

    Mercenary - > Novagen format. Used on all Novagen titles. Also support a format variant on Mercenary III. Include all Novagen titles in the formats file.

--- libdisk/format/amiga/novagen.c
+++ libdisk/format/amiga/novagen.c
@@ -1,24 +1,27 @@
 /*
- * disk/mercenary.c
+ * disk/novagen.c
  * 
- * Custom format as used on Mercenary by Paul Woakes (Novagen).
+ * Custom format as used by Novagen.
  * 
  * Written in 2020 by Keir Fraser
  * 
  * RAW TRACK LAYOUT:
- *  u16 0xa89a :: 1-3 Sync Words
+ *  u16 0xa89a|0x8914 :: 1-3 Sync Words
  *  u8  0x12,0x34,0x56,0x78,cyl
  *  u8  dat[0x1800]
  *  u8  csum_lo, csum_hi
  * 
- * TRKTYP_mercenary data layout:
+ * TRKTYP_novagen data layout:
  *  u8 sector_data[0x1800]
+ *  u8 sync_id
  */
 
 #include <libdisk/util.h>
 #include <private/disk.h>
 
-static void *mercenary_write_raw(
+const static uint16_t syncs[] = { 0xa89a, 0x8914 };
+
+static void *novagen_write_raw(
     struct disk *d, unsigned int tracknr, struct stream *s)
 {
     struct track_info *ti = &d->di->track[tracknr];
@@ -28,10 +31,13 @@ static void *mercenary_write_raw(
         uint8_t dat[0x1800*2], sum[2*2];
         uint16_t csum;
         uint8_t *block;
-        unsigned int i;
+        unsigned int i, sync_id;
 
-        if ((uint16_t)s->word != 0xa89a)
-            continue;
+        for (sync_id = 0; sync_id < ARRAY_SIZE(syncs); sync_id++)
+            if ((uint16_t)s->word == syncs[sync_id])
+                goto found;
+        continue;
+    found:
 
         ti->data_bitoff = s->index_offset_bc - 15;
 
@@ -68,10 +74,12 @@ static void *mercenary_write_raw(
         if (csum != (sum[0] | (sum[1]<<8)))
             continue;
 
-        block = memalloc(ti->len);
+        block = memalloc(ti->len+1);
         memcpy(block, dat, ti->len);
+        block[ti->len++] = sync_id;
         set_all_sectors_valid(ti);
-        ti->total_bits = 102500;
+        stream_next_index(s);
+        ti->total_bits = (s->track_len_bc > 101100) ? 102200 : 100000;
         return block;
     }
 
@@ -79,16 +87,17 @@ fail:
     return NULL;
 }
 
-static void mercenary_read_raw(
+static void novagen_read_raw(
     struct disk *d, unsigned int tracknr, struct tbuf *tbuf)
 {
     struct track_info *ti = &d->di->track[tracknr];
-    uint16_t csum;
+    uint16_t csum, sync;
     uint8_t *dat = (uint8_t *)ti->dat;
     unsigned int i;
 
-    tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0xa89aa89a);
-    tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, 0xa89a);
+    sync = syncs[dat[0x1800]];
+    for (i = 0; i < 3; i++)
+        tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, sync);
     tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 32, 0x12345678);
     tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, tracknr>>1);
     tbuf_bytes(tbuf, SPEED_AVG, bc_mfm, 0x1800, dat);
@@ -101,11 +110,11 @@ static void mercenary_read_raw(
     tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 8, (uint8_t)(csum>>8));
 }
 
-struct track_handler mercenary_handler = {
+struct track_handler novagen_handler = {
     .bytes_per_sector = 0x1800,
     .nr_sectors = 1,
-    .write_raw = mercenary_write_raw,
-    .read_raw = mercenary_read_raw
+    .write_raw = novagen_write_raw,
+    .read_raw = novagen_read_raw
 };
 
 /*
