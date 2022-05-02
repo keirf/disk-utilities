b27d23f135a5634292c1f8b2cfb9538fedf2e758
Keir Fraser <keir.xen@gmail.com>
2022-05-02 16:34:43 +0100

    Robocop: Tidy the handler a little and simplify the format specifier. There really is no checksum of any kind whatsoever. Sad. :(

--- libdisk/format/amiga/robocop.c
+++ libdisk/format/amiga/robocop.c
@@ -1,16 +1,16 @@
 /*
  * disk/robocop.c
  *
- * Custom format as used on Robocop by Ocean.
+ * Custom format as used on original retail release of Robocop by Ocean.
  *
  * Written in 2022 by Keith Krellwitz
  *
  * RAW TRACK LAYOUT:
- *  u16 0x4489 Sync
- *  u32 signature bc_mfm_even_odd = 0x41524230
+ *  u8  0xA1 (4489 Sync)
+ *  u32 0x41524230 ('ARB0')
  *  u32 dat[6224/4]
  *
- * No checksum in data
+ * No checksum of any kind.
  *
  * TRKTYP_robocop data layout:
  *  u8 sector_data[6224]
@@ -18,6 +18,8 @@
 
 #include <libdisk/util.h>
 #include <private/disk.h>
+
+#define SIG_ARB0 0x41524230
 
 static void *robocop_write_raw(
     struct disk *d, unsigned int tracknr, struct stream *s)
@@ -37,7 +39,7 @@ static void *robocop_write_raw(
         if (stream_next_bytes(s, raw, 8) == -1)
             goto fail;
         mfm_decode_bytes(bc_mfm_even_odd, 4, raw, &sig);
-        if (be32toh(sig) != 0x41524230)
+        if (be32toh(sig) != SIG_ARB0)
             continue;
 
         for (i = 0; i < ti->len/4; i++) {
@@ -65,7 +67,7 @@ static void robocop_read_raw(
     unsigned int i;
 
     tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, 0x4489);
-    tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, 0x41524230);
+    tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, SIG_ARB0);
 
     for (i = 0; i < ti->len/4; i++)
         tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, be32toh(dat[i]));
