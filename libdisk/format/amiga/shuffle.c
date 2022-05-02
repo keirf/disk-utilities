7e2e41c79623dda524059a07abce4ee4bc3007d6
Keir Fraser <keir.xen@gmail.com>
2022-05-02 16:04:40 +0100

    Shuffle: Fix checksum calculation and clean up the analyser a little.

--- libdisk/format/amiga/shuffle.c
+++ libdisk/format/amiga/shuffle.c
@@ -3,14 +3,15 @@
  *
  * Custom format as used on Shuffle by Tale.
  *
- * Written in 2022 by Keith Krellwitz
+ * Written in 2022 by Keith Krellwitz & Keir Fraser
  *
  * RAW TRACK LAYOUT:
- *  u16 0x4489 Sync
- *  u32 0x2aaaaaaa
+ *  u8  0xA1 (4489 Sync)
+ *  u16 0
  *  u32 dat[6300/4]
- *
- * Checksum & over decoded data will always be 0
+ *  u16 crc16_ccitt
+ * 
+ * CRC is a duplicator's checksum: It is ignored by the game trackloader.
  *
  * TRKTYP_shuffle data layout:
  *  u8 sector_data[6300]
@@ -26,29 +27,36 @@ static void *shuffle_write_raw(
 
     while (stream_next_bit(s) != -1) {
 
-        uint32_t raw[2], dat[ti->len/4], sum;
+        uint32_t raw[2], dat[ti->len/4];
         unsigned int i;
         char *block;
 
-        if ((uint16_t)s->word != 0x4489)
+        /* sync */
+        if ((s->word>>16) != 0x4489)
             continue;
-        ti->data_bitoff = s->index_offset_bc - 15;
 
-        if (stream_next_bits(s, 32) == -1)
+        ti->data_bitoff = s->index_offset_bc - 31;
+        stream_start_crc(s);
+
+        /* pad */
+        if (stream_next_bits(s, 16) == -1)
             goto fail;
-        if (s->word != 0x2aaaaaaa)
+        if (mfm_decode_word(s->word) != 0)
             continue;
 
-        for (i = sum = 0; i < ti->len/4; i++) {
+        /* data */
+        for (i = 0; i < ti->len/4; i++) {
             if (stream_next_bytes(s, raw, 8) == -1)
                 goto fail;
             mfm_decode_bytes(bc_mfm_even_odd, 4, raw, &dat[i]);
-            sum &= be32toh(dat[i]);
         }
 
-        if (sum != 0)
+        /* crc */
+        if (stream_next_bits(s, 32) == -1)
+            goto fail;
+        if (s->crc16_ccitt != 0)
             continue;
-
+       
         ti->total_bits = 102200;
         block = memalloc(ti->len);
         memcpy(block, dat, ti->len);
@@ -67,11 +75,14 @@ static void shuffle_read_raw(
     uint32_t *dat = (uint32_t *)ti->dat;
     unsigned int i;
 
+    tbuf_start_crc(tbuf);
     tbuf_bits(tbuf, SPEED_AVG, bc_raw, 16, 0x4489);
-    tbuf_bits(tbuf, SPEED_AVG, bc_raw, 32, 0x2aaaaaaa);
+    tbuf_bits(tbuf, SPEED_AVG, bc_mfm, 16, 0);
 
     for (i = 0; i < ti->len/4; i++)
         tbuf_bits(tbuf, SPEED_AVG, bc_mfm_even_odd, 32, be32toh(dat[i]));
+
+    tbuf_emit_crc16_ccitt(tbuf, SPEED_AVG);
 }
 
 struct track_handler shuffle_handler = {
